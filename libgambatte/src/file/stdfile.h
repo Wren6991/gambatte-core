866e4e996e08ee6768110eec14804d534c5e35ee
Luke Wren <wren6991@gmail.com>
2021-08-23 17:38:04 +0100

    Attempt to make stdfile build on both host and device

--- libgambatte/src/file/stdfile.h
+++ libgambatte/src/file/stdfile.h
@@ -25,32 +25,43 @@ Free Software Foundation, Inc.,
 #include "file.h"
 #include <string.h>
 
+#if PICO_ON_DEVICE
 // For panic()
 #include "pico.h"
+#include <cstring>
+#else
+#include <fstream>
+#endif
 
-// This originally used regular C++ fstream. Now uses a static manifest of
-// some blobs in flash (poor man's FS).
+// On-device, we have flash-resident blobs for ROMs etc, and these are used
+// in-place (no room to copy into RAM).
+//
+// Off-device we still use standard C++ file IO, but we take (and leak!) a
+// memory copy internally so that the "in-place" pointer can be passed around
+// like it is on-device.
 
+#if PICO_ON_DEVICE
 // Static file table, ends with sentinel value with null name.
 extern const file_entry file_manifest;
+#endif
 
 namespace gambatte {
 
 class StdFile : public File {
 public:
-	explicit StdFile(char const *filename)
-	: fsize_(0), get_ptr_(0), flash_resource_(0)
+	explicit StdFile(char const *filename):
+#if PICO_ON_DEVICE
+	get_ptr_(0),
+#else
+	stream_(filename, std::ios::in | std::ios::binary),
+#endif
+	fsize_(0), flash_resource_(0)
 	{
+#if PICO_ON_DEVICE
 		int i;
 		for (i = 0; (&file_manifest)[i].name; ++i)
 		{
-			const char *p = (&file_manifest)[i].name, *q = filename;
-			bool match = true;
-			do {
-				match = match && *p == *q;
-			} while (*p++ && *q++);
-
-			if (match) {
+			if (!strcmp((&file_manifest)[i].name, filename)) {
 				printf("Found \"%s\" @%p\n", (&file_manifest)[i].name, (&file_manifest)[i].resource);
 				fsize_ = (&file_manifest)[i].size;
 				flash_resource_ = (&file_manifest)[i].resource;
@@ -60,25 +71,58 @@ public:
 		if (!(&file_manifest)[i].name) {
 			panic("Couldn't find file %s\n", filename);
 		}
+#else
+		if (stream_) {
+			stream_.seekg(0, std::ios::end);
+			fsize_ = stream_.tellg();
+			stream_.seekg(0, std::ios::beg);
+			// Create a memory copy for "zero-copy" use elsewhere. Leak the memory.
+			char *buf = new char[fsize_];
+			stream_.read(buf, fsize_);
+			stream_.seekg(0, std::ios::beg);
+			flash_resource_ = buf;
+		}
+#endif
 	}
 
-	virtual void rewind() { get_ptr_ = 0; }
+	virtual void rewind() {
+#if PICO_ON_DEVICE
+		get_ptr_ = 0;
+#else
+		stream_.seekg(0, std::ios::beg);
+#endif
+	}
 	virtual std::size_t size() const { return fsize_; };
 	virtual void read(char *buffer, std::size_t amount)
 	{
+#if PICO_ON_DEVICE
 		if (fsize_ - get_ptr_ < amount)
 			amount = fsize_ - get_ptr_;
 		memcpy(buffer, flash_resource_, amount);
 		get_ptr_ += amount;
+#else
+		stream_.read(buffer, amount);
+#endif
 	}
-	virtual bool fail() const { return flash_resource_ == NULL; }
+	virtual bool fail() const {
+#if PICO_ON_DEVICE
+		return flash_resource_ == NULL;
+#else
+		return stream_.fail();
+#endif
+	}
+
 	// Get pointer to raw flash resource, for zero-copy read-only use
 	virtual const char* getraw() const {return flash_resource_;}
 
 private:
-	std::size_t fsize_;
+#if PICO_ON_DEVICE
 	size_t get_ptr_;
+#else
+	std::ifstream stream_;
+	std::size_t fsize_;
 	const char *flash_resource_;
+#endif
 };
 
 }
